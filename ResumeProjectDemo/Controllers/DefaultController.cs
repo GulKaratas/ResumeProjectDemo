4a79475b8a9c0feab7c5aaede32cdfcdfe685cd9
GulKaratas <glsrn_7872@outlook.com>
2026-01-27 01:23:09 +0300

    messages saved in database

--- ResumeProjectDemo/Controllers/DefaultController.cs
+++ ResumeProjectDemo/Controllers/DefaultController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProjectDemo.Context;
+using ResumeProjectDemo.Entities;
+
 
 namespace ResumeProjectDemo.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly ResumeContext _context;
+
+        public DefaultController(ResumeContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] Message message)
+        {
+            try
+            {
+                message.SendDate = DateTime.Now;
+                message.IsRead = false;
+
+                if (string.IsNullOrEmpty(message.Subject))
+                {
+                    message.Subject = "Konu Yok";
+                }
+                _context.Messages.Add(message);
+                _context.SaveChanges();
+
+                return Json(new { success = true, message = "Mesajınız başarıyla gönderildi." });
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "Hata oluştu!" });
+            }
         }
     }
 }
